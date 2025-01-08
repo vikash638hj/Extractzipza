d4eb79f1934234aabdb8026d038105a29ff0c066
krishan041 <krishandara04@gmail.com>
2025-01-09 01:21:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -252,7 +252,7 @@ async def account_login(bot: Client, m: Message):
                 cmd= f" yt-dlp -k --allow-unplayable-formats -f bestvideo.{quality} --fixup never {url} "
                 print("counted")
 
-            else https://appx1.arvind.eu.org/.../main.m3u8
+            else https//appx1.arvind.eu.org/.../main.m3u8
             
 
             if "edge.api.brightcove.com" in url:
